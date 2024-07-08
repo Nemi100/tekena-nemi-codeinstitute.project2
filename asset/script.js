54e33c713dcde278a901b20db41ec22b5209b171
Tekena Nemi <teksity@gmail.com>
2024-07-08 23:32:39 +0100

    initial commit

--- asset/script.js
+++ asset/script.js
@@ -1,36 +1,48 @@
-document.onload = console.log('The document has loaded!');
+document.onload = console.log("The document has loaded!");
 
 let buttons = document.getElementsByTagName("button");
 
 
 
 function bodyLoaded() {
-    console.log('Body has loaded!');
+    console.log("Body has loaded!");
 }
 
 
 let holidayChoices=[
-                    {location: 'Devon',price   : '£500',choice:'coast',tel :'07245368192',},
-                    {location: 'Brighton',price    : '£650',choice: 'city',tel :'07839280920',},
-                    {location: 'Leeds',price    : '£410',choice:'city',tel :'07297102941',},
-                    {location: 'Manchester',price    : '£350',choice:'coast',tel :'07889201975',},
-                    {location: 'Oxford',price    : '£730',choice:'city',tel :'07278390265',},
-                    {location: 'Bristol',price    : '£300',choice:'coast',tel :'07210927354',},
-                    {location: 'liverpool',price    : '£500',choice : 'coast',tel :'07753467425',},
-                    {location: 'Belfast',price    : '£410',choice:'city',tel :'07297102941',},
-                    {location: 'Nottingham',price    : '£350',choice:'coast',tel :'07889201975',},
-                    {location: 'Plymouth',price    : '£730',choice:'city',tel :'07278390265',},
-                    {location: 'Newcastle',price    : '£300',choice:'coast',tel :'07210927354',},
-                    {location: 'Birmingham',price    : '£500',choice : 'city',tel :'07753467425',},
-                ]
+    {Preference:"coast",location: "Devon",
+        price: "£500",tel :"07245368192"},
+    {Preference: "city",location: "Brighton",
+        price: "£650",tel :"07839280920"},
+    {Preference:"city",location: "Leeds",
+        price: "£410",tel :"07297102941"},
+    {Preference:"coast",location: "Manchester",
+        price:"£350",tel :"07889201975"},
+    {Preference:"city",location: "Oxford",
+        price:"£730",tel :"07278390265"},
+    {Preference:"coast",location: "Bristol",
+        price:"£300",tel :"07210927354"},
+    {Preference :"coast",location: "liverpool",
+        price:"£500",tel :"07753467425"},
+    {Preference:"city",location: "Belfast",
+        price: "£410",tel :"07297102941"},
+    {Preference:"coast",location: "Nottingham",
+        price:"£350",tel :"07889201975"},
+    {Preference:"city",location: "Plymouth",
+        price:"£730",tel :"07278390265"},
+    {Preference:"coast",location: "Newcastle",
+        price:"£300",tel :"07210927354"},
+    {Preference :"city",location:"Birmingham",
+        price:"£500",tel :"07753467425"}
+];
 
 console.log(holidayChoices);
 
 
 function buildTable(data){
-    var table = document.getElementById("myTable")
+    let table = document.getElementById("myTable");
     table.innerHTML = "";
-    for(var i = 0;i < data.length; i++) {
+    for(let i = 0;i < data.length; i++) {
         var row = `<tr>
                      <td>${data[i].location}</td>
                      <td>${data[i].price}</td>
@@ -42,11 +54,11 @@ function buildTable(data){
 
 
     let coast=holidayChoices.filter((holidayChoices) => {
-        return holidayChoices.choice === "coast"});
+        return holidayChoices.Preference === "coast"});
         console.log(coast);
 
     let city = holidayChoices.filter((holidayChoices) => {
-        return holidayChoices.choice === "city"});
+        return holidayChoices.Preference === "city"});
         console.log(city);
   
 /**
@@ -59,17 +71,17 @@ function handleSubmit() {
     
 
     // Get the two input elements
-    var rd1 = document.getElementById('rd1');
-    var rd2 = document.getElementById('rd2');
+    var rd1 = document.getElementById("rd1");
+    var rd2 = document.getElementById("rd2");
 
     if (rd1.checked == true ){
-        console.log('coast:', rd1.value);
+        console.log("coast:", rd1.value);
         buildTable(coast);
         document.querySelector(".result-area").style.display ="block";
 
     }
     else if(rd2.checked==true){
-        console.log('city:', rd2.value);
+        console.log("city:", rd2.value);
         buildTable(city);
         document.querySelector(".result-area").style.display ="block";
     }
